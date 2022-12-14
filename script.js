53f90ebeeab5f39b354830bdc2d44d25ce039ce2
chicken nugget <116309083+chickennu99et@users.noreply.github.com>
2022-12-14 14:47:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function myFunction() {
-  var x = document.getElementById("myDIV");
+function l() {
+  var x = document.getElementById("1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function ll() {
+  var x = document.getElementById("2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function lll() {
+  var x = document.getElementById("3");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
